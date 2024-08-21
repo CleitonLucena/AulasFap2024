5ee6461b6bdfc7fdaefa7b1747bfc3fe5f8f3551
Cleiton Lucena <107970607+CleitonLucena@users.noreply.github.com>
2024-08-21 18:59:35 -0300

    Atualizei

--- objetos.js
+++ objetos.js
@@ -1,12 +1,12 @@
 class ContaBancaria {           /*Nome do objeto*/
 
     constructor(){
-        this.Agencia = 123-4    /*Caracteristicas do objeto*/
-        this.NumeroConta = 1 
-        this.Saldo = 1000
-        this.Limite = 500
+        this.agencia = 123-4    /*Caracteristicas do objeto*/
+        this.numeroConta = 1 
+        this.saldo = 1000
+        this.limite = 500
     }
-}
+
 
 depositar(valorDeposito){
     this.saldo += valorDeposito
@@ -28,9 +28,10 @@ emprestimo(){
 limite() {
 
 }
-
+}
 
 let conta1 = new ContaBancaria
 let conta2 = new ContaBancaria
+
 console.log(conta1.verificarSaldo())
 
