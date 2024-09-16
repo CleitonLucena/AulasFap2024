760b1d4009039d38737dbeed6dee1fd18c0ed57b
Cleiton Lucena <cleitonlucena21@gmail.com>
2024-09-16 20:11:58 -0300

    código do morcego

--- typescript.ts
+++ typescript.ts
@@ -1,9 +1,65 @@
-class Relogio {
-    protected marcaHoras : string;
+class Animal {     /*Classe pai*/
+    cor : string
+    idade: number
+    boca: string
 
-    constructor(marcaHoras : string){
-        this.marcaHoras = marcaHoras
+    constructor(){
+        this.cor = 'Preto'
+        this.idade = 5
+        this.boca = 'Grande'
+    }
+
+    Correr() : void
+    {
+        console.log('Correndo')
+    }
+
+    Dormir() : void
+     {
+        console.log('Dormindo')
+    }
+}
+
+class Cachorro extends Animal {   /*Classe Filho*/
+    constructor() {
+        super()
+    }
+}
+
+class Gato extends Animal {
+    constructor() {
+        super()
     }
 }
 
 
+class Morcego extends Animal {
+    constructor() {
+        super()
+    }
+
+
+    Dormir() : void
+    {
+       console.log('Dorme de cabeça para baixo')
+   }
+
+
+    Voar() : void {
+        console.log('Voa rápido')
+    }
+}
+
+
+let cachorro = new Cachorro()
+cachorro.Correr()
+cachorro.Dormir()
+
+let gato = new Gato()
+cachorro.Correr()
+cachorro.Dormir()
+
+let morcego = new Morcego()
+morcego.Correr()
+morcego.Dormir()
+morcego.Voar()
